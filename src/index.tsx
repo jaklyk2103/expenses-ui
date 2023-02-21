0eb7363d89517d9cdbc5403c68e369194bbd73b5
Jakub Lyko <kubeckk21@gmail.com>
2023-02-21 15:23:51 +0100

    fixing errors

--- src/index.tsx
+++ src/index.tsx
@@ -2,21 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyA1Ffha7B1vVWVqorhIVFVfhKECpvlMKxk",
-  authDomain: "expenses-8c745.firebaseapp.com",
-  projectId: "expenses-8c745",
-  storageBucket: "expenses-8c745.appspot.com",
-  messagingSenderId: "513889353253",
-  appId: "1:513889353253:web:86728a63be6392a4ab09cc",
-  measurementId: "G-D15DT6W2QC"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyA1Ffha7B1vVWVqorhIVFVfhKECpvlMKxk",
+//   authDomain: "expenses-8c745.firebaseapp.com",
+//   projectId: "expenses-8c745",
+//   storageBucket: "expenses-8c745.appspot.com",
+//   messagingSenderId: "513889353253",
+//   appId: "1:513889353253:web:86728a63be6392a4ab09cc",
+//   measurementId: "G-D15DT6W2QC"
+// };
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app); // eslint-disable-line no-unused-vars
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app); // eslint-disable-line no-unused-vars
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
