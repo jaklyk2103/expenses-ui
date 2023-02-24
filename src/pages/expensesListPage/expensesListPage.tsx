29d74c184c70c9f24da844b6a89204c76e9c5884
Jakub Lyko <kubeckk21@gmail.com>
2023-02-24 17:30:59 +0100

    Fixed lint problem

--- src/pages/expensesListPage/expensesListPage.tsx
+++ src/pages/expensesListPage/expensesListPage.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import ExpensesList from '../../components/expensesList/expensesList';
 import { Expense } from '../../services/types';
 import { ExpensesService } from '../../services/expensesService';
-import SingleNavigationLink from '../../components/singleNavigationLink/singleNavigationLink';
 import NavigationBar from '../../components/navigationBar/navigationBar';
 
 const ExpensesListPage = () => {
