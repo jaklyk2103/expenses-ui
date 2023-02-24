658bf0c7740d4cef553bd1efcc385c95d2711ed9
Jakub Lyko <kubeckk21@gmail.com>
2023-02-24 15:47:54 +0100

    Styled navigation bar

--- src/components/navigationBar/navigationBar.tsx
+++ src/components/navigationBar/navigationBar.tsx
@@ -11,7 +11,7 @@ export default function NavigationBar(props: NavigationBarProps) {
   return(
     <div className='navigation-bar-wrapper'>
       {props.leftNavigationBarItem && <Link className='single-navigation-link navigation-bar-link-left' {...props.leftNavigationBarItem}>{props.leftNavigationBarItem.children}</Link>} 
-      <div><p className='navigation-bar-page-title'>{props.pageTitle}</p></div>
+      <div className='navigation-bar-page-title'><p>{props.pageTitle}</p></div>
       {props.rightNavigationBarItem && <Link className='single-navigation-link navigation-bar-link-right' {...props.rightNavigationBarItem}>{props.rightNavigationBarItem.children}</Link>} 
     </div>
   )
