7bce347c8c4e64666d00104d3163e5f0c1b139df
Jakub Lyko <kubeckk21@gmail.com>
2023-02-27 21:10:57 +0100

    Trying to add autocompletion support to the login form

--- src/components/loginForm/loginForm.tsx
+++ src/components/loginForm/loginForm.tsx
@@ -30,8 +30,10 @@ export default function LoginForm() {
 
   return(
     <form>
-      <input type="email" onChange={handleEmailChange} placeholder='Email' />
-      <input type="text" onChange={handlePasswordChange} placeholder='Password' />
+      <label htmlFor="email">Email</label>
+      <input name="email" autoComplete='email' type="email" onChange={handleEmailChange} placeholder='Email' />
+      <label htmlFor="password">Password</label>
+      <input name="password" autoComplete='current-password' type="password" onChange={handlePasswordChange} placeholder='Password' />
       <button onClick={handleLoginButtonClick}>Log in</button>
     </form>
   );
